e849a44249e768926b8e5c639aef57f4a64e0fac
dave <daveb@ekconsulting.net>
2020-02-18 16:55:00 +0800

    added infinite scroll feature

--- src/container/product.js
+++ src/container/product.js
@@ -1,15 +1,18 @@
 import React, { useCallback, useState, useEffect } from 'react';
 
+import useEventListener from '../component/use-event-listener';
 import Sort from '../component/sort';
 import ProductList from '../component/product-list';
 
 const ProductContainer = () => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const response = await fetch('http://localhost:3002/products?_sort=size&_order=desc');
+        const response = await fetch(`http://localhost:3002/products?_sort=size&_order=desc&_page=${page}&_limit=10`);
         const products = await response.json();
         setProducts(products)
       }catch(error){
@@ -19,14 +22,38 @@ const ProductContainer = () => {
     fetchProducts();
   }, []);
 
+  const handleScroll = () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      if(!loading){
+        setPage(page+1);
+      }
+    }
+  };
+  useEventListener("scroll", handleScroll);
+
+  useEffect(() => {
+    setLoading(true);
+    getProducts();
+  }, [page]);
+
   const setFilter = useCallback(async (state) => {
     try {
-      const response = await fetch(`http://localhost:3002/products?_sort=${state.item}&_order=${state.order}`);
+      const response = await fetch(`http://localhost:3002/products?_sort=${state.item}&_order=${state.order}&_page=10&_limit=10`);
       const products = await response.json();
       setProducts(products)
     }catch(error){
     }
   }, []);
+
+  const getProducts = useCallback(async () => {
+    try {
+      const response = await fetch(`http://localhost:3002/products?_page=${page}&_limit=10`);
+      const _products = await response.json();
+      setProducts([...products, ..._products]);
+      setLoading(false);
+    }catch(error){
+    }
+  });
 
   return (
     <>
